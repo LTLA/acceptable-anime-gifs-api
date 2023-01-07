7a63221bf8d57f0fa776417ee8528a3b9152337c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-07 22:31:07 +1100

    Bugfixes for the random endpoints.

--- src/index.js
+++ src/index.js
@@ -149,29 +149,33 @@ router.put('/index', async (request, env, context) => {
 /*************************************************
  *************************************************/
 
-function choose_random_gif(query) {
+function choose_random_gif(query, env) {
     let rquery = "SELECT gifs.gif_path, gifs.show_id FROM gifs";
     let prework = [];
  
     // Possibly restricting by show.
     if ("shows" in query) {
-        let allowed = Array.from(new Set(query["shows"].split(",")));
+        prework.push(env.DB.prepare("DROP TABLE IF EXISTS restricted_shows"));
         prework.push(env.DB.prepare("CREATE TEMPORARY TABLE restricted_shows (show_id TEXT PRIMARY KEY)"));
+
+        let allowed = Array.from(new Set(query["shows"].split(",")));
         for (const x of allowed) {
             prework.push(env.DB.prepare("INSERT into restricted_shows (show_id) VALUES (?)").bind(x));
         }
-        rquery += "\nINNER JOIN restricted_shows ON restricted_shows.show_id == gifs.show_id";
+        rquery += "\nINNER JOIN restricted_shows ON restricted_shows.show_id = gifs.show_id";
     }
 
     // Possibly restricting by sentiment.
     if ("sentiments" in query) {
-        let allowed = Array.from(new Set(query["sentiments"].split(",")));
+        prework.push(env.DB.prepare("DROP TABLE IF EXISTS restricted_sentiments"));
         prework.push(env.DB.prepare("CREATE TEMPORARY TABLE restricted_sentiments (sentiment TEXT PRIMARY KEY)"));
+
+        let allowed = Array.from(new Set(query["sentiments"].split(",")));
         for (const x of allowed) {
             prework.push(env.DB.prepare("INSERT into restricted_sentiments (sentiment) VALUES (?)").bind(x));
         }
-        rquery += "\nINNER JOIN gif_sentiment ON gif_sentiment.gif_path == gifs.gif_path";
-        rquery += "\nINNER JOIN restricted_sentiments ON restricted_sentiments.sentiment == gif_sentiment.sentiment";
+        rquery += "\nINNER JOIN gif_sentiment ON gif_sentiment.gif_path = gifs.gif_path";
+        rquery += "\nINNER JOIN restricted_sentiments ON restricted_sentiments.sentiment = gif_sentiment.sentiment";
     }
 
     rquery += "\nORDER BY RANDOM() LIMIT 1";
@@ -186,16 +190,16 @@ function choose_random_gif(query) {
 }
 
 async function choose_random_gif_with_details(query, env) {
-    let choice = choose_random_gif(query);
+    let choice = choose_random_gif(query, env);
     let offset = choice.length - 1;
 
     let statements = [
         ...choice,
         env.DB.prepare(`SELECT show_name, shows.show_id FROM shows 
-INNER JOIN random ON shows.show_id == random.show_id`),
+INNER JOIN random ON shows.show_id = random.show_id`),
         env.DB.prepare(`SELECT character_name, characters.character_id FROM characters 
-INNER JOIN character_gif ON characters.character_id == character_gif.character_id
-INNER JOIN random ON character_gif.gif_path == random.gif_path`)
+INNER JOIN character_gif ON characters.character_id = character_gif.character_id
+INNER JOIN random ON character_gif.gif_path = random.gif_path`)
     ];
 
     // Extracting useful details.
@@ -206,14 +210,17 @@ INNER JOIN random ON character_gif.gif_path == random.gif_path`)
     }
     
     let gif_path = res[offset].results[0].gif_path;
-    let show_info = res[offset + 1].results;
-    if (show_info.length != 1) {
+    let show_details = res[offset + 1].results;
+    if (show_details.length != 1) {
         throw new Error("expected exactly one show for the selected GIF '" + gif_path + "'");
     }
+    let show_info = show_details[0];
 
     let character_info = res[offset + 2].results;
     return { gif_path, show_info, character_info };
 }
+
+const base_url = "https://raw.githubusercontent.com/LTLA/acceptable-anime-gifs/master/registry/";
 
 router.get("/random", async (request, env, context) => {
     var chosen = await choose_random_gif_with_details(request.query, env)
@@ -255,7 +262,7 @@ router.get("/random/markdown", async (request, env, context) => {
 })
 
 router.get("/random/gif", async (request, env, context) => {
-    var statements = choose_random_gif(request.query)
+    var statements = choose_random_gif(request.query, env)
     let offset = choice.length - 1;
 
     let res = await env.DB.batch(statements);
